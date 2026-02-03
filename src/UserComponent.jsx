75d6ae26503cdf4be83b3485b7bee09e1476ee87
Ayush Prajapati <prajapatiayush426@gmail.com>
2026-02-03 17:52:52 +0530

    first commit

---
+++ src/UserComponent.jsx
@@ -0,0 +1,17 @@
+function Login() {
+  return <h1>Login User</h1>;
+}
+
+export default Login;
+
+export function Profile() {
+  return <h1>Profile</h1>;
+}
+
+export function Setting() {
+  return(
+    <div>
+        <h1>Setting</h1>;
+    </div>
+  )
+}
