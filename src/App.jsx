c69cb938cd815be7a1c3fcdc26c7162701c62501
Ayush Prajapati <prajapatiayush426@gmail.com>
2026-02-05 14:13:19 +0530

    Props in React JS

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,31 @@
-import {useState} from "react";
-import User from "./User.jsx"
+import User from "./User"
+import College from "./College"
+
 function App(){
-  const[display,setDisplay]=useState(true)
+  // let userName="Ayush Prajapati";
+  // let age=23;
+  // let email="ayush@test.com"
+  let userObject1={
+    name:"Ayush Prajapati",
+    age:29,
+    email:"ayush@test.com"
+  }
+  let userObject2={
+    name:"Mohit",
+    age:18,
+    email:"mohit@test.com"
+  }
+  let collegeName=['IET','DU','IIT','NIT','MIT']
+
   return(
     <div>
-      <h1>Toggle in React JS</h1>
-      <button onClick={()=>setDisplay(!display)}>Toggle</button>
-      {/* {display?<h1>Ayush Prajapati</h1>:null}*/}
-      {
-        display?<User/>:null
-      }
-      
+      <h1>Props in React Js</h1>
+      <College name={collegeName}/>
+       
+        {/* <User name={userName} age={age} email={email}/> */}
+        <User user={userObject1} />
+         <User user={userObject2} />
     </div>
   )
 }
-export default App;  
+export default App;
--- src/User.jsx
+++ src/User.jsx
@@ -1,10 +1,12 @@
-
-function User(){
-
-    return((
+function User({user}){
+    
+    return(
         <div>
-            <h1>Ayush Prajapati</h1>
+            <hr/>
+            <h2>Name:{user.name}</h2>
+            <h2>Age:{user.age}</h2>
+            <h3>Email:{user.email}</h3>
         </div>
-    ))
+    )
 }
 export default User;
