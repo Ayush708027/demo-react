ed21b763a047e615c71e0e87676085ac8501166b
Ayush Prajapati <prajapatiayush426@gmail.com>
2026-02-05 22:02:03 +0530

    Handle Checkboxes

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,9 @@
-import {useState} from "react"
+import Skills from "./Skills";
 function App(){
-  const[name,setName]=useState('')
-  const[password,setPassword]=useState('')
-  const[email,setEmail]=useState('')
   return(
     <div>
-      <h1>Controller Component </h1>
-
-      <form action="" method="get">
-
-        <input type="text" value={name} onChange={(event)=>setName(event.target.value)} placeholder="Enter Name"/>
-        <br />< br/>
-
-         <input type="password" value={password} onChange={(event)=>setPassword(event.target.value)} placeholder="Enter password"/>
-        <br />< br/>
-
-         <input type="text" value={email} onChange={(event)=>setEmail(event.target.value)} placeholder="Enter Email"/>
-        <br />< br/>
-
-        <button>Submit</button>
-
-        <h3>{name}</h3>
-        <h3>{password}</h3>
-        <h3>{email}</h3>
-        
-        <button onClick={()=>{setEmail('');setName('');setPassword('')}}>clear</button>
-      </form>
+      <h1>Handle Checkbox in React JS</h1>
+      <Skills/>
     </div>
   )
 }
