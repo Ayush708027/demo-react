c433edc486d231da519208489ecb23e0501014af
Ayush Prajapati <prajapatiayush426@gmail.com>
2026-02-06 14:45:58 +0530

    Loop in JSX with Map Function

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,107 @@
-import {useState} from "react";
 function App(){
-  const[gender,setGender]=useState('male')
-  const[city, setCity]=useState("NewDelhi")
+  const userName=['anil','sam','peter','bruce']
+
+  const userData=[
+    { 
+      id:1,
+      name:'Anil',
+      age:'29',
+      email:"anil@test.com",
+      
+    },
+    {
+      id:2,
+      name:'Sam',
+      age:'23',
+      email:"sam@test.com",
+      
+    },
+    {
+      id:3,
+      name:'Peter',
+      age:'19',
+      email:"peter@test.com",
+     
+    },
+    {
+      id:4,
+      name:'Bruce',
+      age:'30',
+      email:"bruce@test.com",
+      
+    }
+  ]
   return(
     <div>
-      <h1>Handle Radio and Dropdown</h1>
-      <h3>Select Gender</h3>
-      <input type="radio" onChange={(event)=>setGender(event.target.value)} name="gender" value={"Male"} checked={gender=='male'} id="male"/> 
-      <label htmlFor="male">Male</label>
-      <input type="radio" onChange={(event)=>setGender(event.target.value)} name="gender"  value={"Female"} checked={gender == 'female'} id="female"/>
-       <label htmlFor="female">Female</label>
-
-       <h2>Selected Gender:{gender}</h2>
-       <br/> <br/> <br/>
-       <h4>Select City</h4>
-       <select onChange={(event)=>setCity(event.target.value)} defaultValue={"NewDelhi"}>
-        <option value="noida">Noida</option>
-        <option value="gurgaon">Gurgaon</option>
-        <option value="NewDelhi">NewDelhi</option>
+      <h1>Loop in JSX with Map Function</h1>
+      <table border="1">
+        <thead>
+          <tr>
+            <td>Id</td>
+            <td>Name</td>
+            <td>Age</td>
+            <td>Email</td>
+           
+          </tr>
+        </thead>
+        <tbody>
+          {
+            userData.map((user)=>(
+            <tr>
+              <td>{user.id}</td>
+              <td>{user.name}</td>
+              <td>{user.age}</td>
+              <td>{user.email}</td>
+           
+              
+            </tr>
+            ))
+          }
+        </tbody>
+      </table>
+      <h1>Dummy Data</h1>
+      <table border="1" cellSpacing="0">
+        <thead>
+          <tr>
+            <td>Id</td>
+            <td>Name</td>
+            <td>Age</td>
+            <td>Email</td>
         
-       </select>
-       <h2>Selected City:{city}</h2>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Anil</td>
+            <td>29</td>
+            <td>anil@test.com</td>
+           
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Sam</td>
+            <td>23</td>
+            <td>sam@test.com</td>
+            
+          </tr>
+          <tr>
+             <td>3</td>
+            <td>Peter</td>
+            <td>19</td>
+            <td>peter@test.com</td>
+           
+          </tr>
+          <tr>
+            <td>4</td>
+            <td>Bruce</td>
+            <td>30</td>
+            <td>bruce@test.com</td>
+            
+          </tr>
+        </tbody>
+      </table>
     </div>
-    
   )
 }
 export default App;
