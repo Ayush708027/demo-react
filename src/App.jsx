1aa1457ab34f83e08df4c201133d7abf4e5ad33f
Ayush Prajapati <prajapatiayush426@gmail.com>
2026-02-10 10:56:01 +0530

    Inline Style in React Js

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,41 @@
-import {useEffect} from "react"
-import {useState} from "react";
+import './App.css'
+
 function App(){
-const[counter,setCounter]=useState(0);
-const[data,setData]=useState(0);
-
-useEffect(()=>{
-    callOnce();
-},[counter,data])
-
-  function callOnce(){
-    console.log("callOnce function called");
-
-  }
- 
- 
+  const cardStyle={
+          border: " 1px solid #cccccc3b",
+          width:"200px",
+          boxShadow:"1px 2px 3px 0px #cccccc57",
+          margin:"10px"
+        }
   return(
     <div>
-      <h1>useEffect Hook</h1>
-      <button onClick={()=>setCounter(counter+1)}>Counter{counter}</button>
-      <button onClick={()=>setData(data+1)}>Data{data}</button>
-    </div>  
+      
+      <h1 style={{color:'red', backgroundColor:"black"}}>Inline Style in React Js </h1>
+    <div style= {{display:"flex"}}>
+      <div style={cardStyle}>
+        <img style={{width:"200px"}} src="https://www.w3schools.com/howto/img_avatar.png" alt="" srcset="" />
+        <div style={{padding:"5px"}}>
+          <h4>Ayush Prajapati</h4>
+          <p>Software Developer</p>
+        </div>
+      </div>
+      <div style={cardStyle}>
+        <img style={{width:"200px"}} src="https://www.w3schools.com/howto/img_avatar.png" alt="" srcset="" />
+        <div style={{padding:"5px"}}>
+          <h4>Rohit Singh</h4>
+          <p>Software Developer</p>
+        </div>
+      </div>
+      <div style={cardStyle}>
+        <img style={{width:"200px"}} src="https://www.w3schools.com/howto/img_avatar.png" alt="" srcset="" />
+        <div style={{padding:"5px"}}>
+          <h4>Amit Yadav</h4>
+          <p>Software Developer</p>
+        </div>
+      </div>
+    </div>
+      
+    </div>
   )
 }
-export default App;
+export default App
